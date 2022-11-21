6a921f877aeaa11d81efea7b0d43e238aeb226a5
Max Edlund <maxedlund88@gmail.com>
2022-11-21 14:59:14 +0100

    uppd. pseudokod och lagt till jul/lucia-funktion

--- js/regler.js
+++ js/regler.js
@@ -1,22 +1,39 @@
               /******** PSEUDOKOD ********/
 
-/* Om kunden inte har lagt beställningen inom 15 minuter så ska beställningsformuläret 
-tömmas/rensas och kunden ska meddelas att denne är för långsam.     <-- när  triggas detta? */
+  /* event när sidan laddas */
 
-// om beställningsobjektets totalpris blir större än 800, inaktivera val för faktura  <-- när  triggas detta?
+// spara aktuell dag i variabel   ☑️
+// spara aktuell tidpunkt i variabel  ☑️
 
-// checkForDiscount(price) placeOrder(checkForDiscount()) när man klickar på varukorgssammanställning -> 
-// funktion för att hindra att beställningen ändras, hur gör vi? när? 
-// spara dag för beställning i variabel   ☑️
-// spara tidpunkt för beställning i variabel  ☑️
-// hämta totalpris, spara i variabel  ☑️
+// OM det är 13e december:
+//    lägg till en gratis pepparkakschoklad i kundkorgen eller nåt 🍪😃
+
+// OM det är 24 december
+//    gör pristexter röda
+//    byt bakgrundsbild
 
 
+  /* event på plus och minus-knappar: */
 
-// ska rabatter gå att kombinera? 🤔
+// hämta totalpris från beställning, spara i variabel  ☑️
 
-// kontrollera ifall antal av en chokladsort är beställd mer än 10 st ->
-//    bör göras innan nedan rabatter på totalsumma isf, vart och när?
+    // funktion för att hindra att beställningen ändras, hur gör vi? 
+
+// OM timer för beställning redan startad
+//    Avbryt timer
+//
+// Starta timer för beställning, 15 minuter
+//    OM(eller när? 🤔) tiden tar slut
+//        avbryt timer
+//
+// OM beställningsobjektets totalpris blir större än 800, inaktivera val för faktura  
+// 
+// OM en chokladsort är antal beställda: 10 st eller fler     <- event bara på plus-knappar?
+//    pris-= -10%
+//
+// OM chokladsort antal beställda : färre än 10               <- event bara på minus-knappar?
+//    pris = orginalpriset
+
 
 // Kontrollera ifall det är en rabattvärdig dag, OM sant:   ☑️
 //    OM det är måndag OCH timvariabeln är mindre än 10:    ☑️
@@ -26,42 +43,52 @@ tömmas/rensas och kunden ska meddelas att denne är för långsam.     <-- när
 //        OM jämn vecka && beställningen över 25kr:
 //            subtrahera 25 kr fr totalpris
 
-// OM det är 13e december:
-//    lägg till en gratis pepparkakschoklad eller nåt 🍪😃
+// funktion för att räkna ut frakt:
 
-// OM det är 24 december
-//    gör pristexter röda
-//    byt bakgrundsbild
+// OM antal beställda produkter > 15
+//    frakt = 0
+// ANNARS 
+//    frakt = pris + (pris * 0.1)
+// skriv ut frakt nånstans
 
 // rabattkod, nåt med RegEx? kmr vecka 3 har jag för mig
+
+// uppdatera priset efter alla rabatter och sånt till kundkorg 
 
 // massa matte på leveranser
 
 
               /******** VARIABLER ********/
 
-
-const timeOfOrder = new Date();                     //  bör va typ event på beställningsknapp som kör typ placeOrder(), skapa ett Date-objekt
-const displayDate = timeOfOrder.toDateString();     //  för utskrift av datum och tid för beställning
-let deliveryTime;                                   //  massa matte på timeOfOrder sen
+const timeOfOrder = new Date();                     //  skapa ett Date-objekt
+const dateString = timeOfOrder.toDateString();      //  för kontroll av dag, här eller deklarera i funktionen? isHoliday()?
 const orderDay = timeOfOrder.getDay();              //  spara dagen för beställning som number mellan 0 och 6 (0 = söndag)
 const orderHour = timeOfOrder.getHours();           //  spara klockslag för beställning, number mellan 0 och 23
-const orderWeek = timeOfOrder.getWeek();  // <- hur?
-const initalPrice = customerOrder.totalPrice;        //  hämta totalpriset från kundkorg, innan rabatter/påslag
+// const orderWeek = timeOfOrder.getWeek();  // <- hur?
+// const initalPrice = customerOrder.totalPrice;        //  hämta totalpriset från kundkorg, innan rabatter/påslag
 
+let deliveryTime;                                   //  massa matte på timeOfOrder sen?
 
 
               /********  FUNKTIONER ********/
+/**
+ * testar om det är lucia eller julafton (funktionen verkar funka som den ska!)
+ */
+function isHoliday() {
+  let isXmas = dateString.includes('Dec 24');   // .includes() returnerar true eller false
+  let isLucia = dateString.includes('Nov 22')
 
-
-function ISO8601_week_no(dt) 
-  {
-    // ska prova en funktion från internet
+  if (isXmas) {
+    // TODO: gör pristexter röda och byt bakgrundsbild
   }
+  else if (isLucia) {
+    // TODO: lägg till en gratis lucia-choklad i kundkorg 🍫
+  }
+}
 
 /**
  * kollar om dagen är rabattberättigad
- * @param {number} weekDay - dagen som beställningen las
+ * @param {number} weekDay - dagen sidan laddats på
  * @returns en bool!
  */
 function isDiscountDay(weekDay) {       
@@ -70,6 +97,40 @@ function isDiscountDay(weekDay) {
   }
   else { 
     return false;
+  }
+}
+
+/**
+ * kontrollerar vilken dag beställning lagts
+ */
+function setDayDiscount() {    // tänkte fel från början, bör göras om, applyWeekendIncrease() ska inte vara på totalsumma, utan priset innan frakt (tolkar jag det som? 🤔)
+  
+  switch (orderDay) {           
+    case 1:   // OM måndag:
+    if (orderHour < 10 && orderHour >= 3) {
+    applyMondayDiscount(initalPrice);
+    }
+    else if(orderHour > 0 && orderHour <= 3) { // tänk om
+    applyWeekendIncrease(initalPrice);
+    }
+
+    break;
+
+    case 2:   // OM tisdag
+    checkWeek(orderWeek);   // TODO
+    break;
+
+    case 5:   // OM fredag:
+    if (orderHour > 14) {
+    applyWeekendIncrease(initalPrice);
+  }
+
+  break;
+
+  case 6:   // OM lördag eller söndag:
+  case 0:   
+  applyWeekendIncrease(initalPrice);
+  break;
   }
 }
 
@@ -96,44 +157,13 @@ function applyWeekendIncrease() {
   // sneaky prishöjning
 }
 
-
               /******** PROGRAMFLÖDE ********/ 
 
-
-// startas av typ checkForDiscount() när man klickar på lägg beställning ?
-
+isHoliday();
 isDiscountDay(orderDay);        // kontrollera om dagens dag ger rätt till rabatt eller inte
 
 if (isDiscountDay) {            // om den gör det, kolla vilken dag, och vilken tid
-  switch (orderDay) {           // svårt att kontrollera om detta funkar som det ska, kan ju bara göras på rätt dag och tid 🤔
-    
-    case 1:   // OM måndag:
-      if (orderHour < 10 && orderHour > 3) {
-        applyMondayDiscount(initalPrice);
-      }
-      else if(orderHour > 0 && orderHour <= 3) {
-        applyWeekendIncrease(initalPrice);
-      }
-
-    break;
-
-    case 2:   // OM tisdag
-      checkWeek(orderWeek);
-    break;
-
-    case 5:   // OM fredag:
-      if (orderHour > 14) {
-        applyWeekendIncrease(initalPrice);
-      }
-
-    break;
-
-    case 6:   // OM lördag eller söndag:
-    case 0:   
-      applyWeekendIncrease(initalPrice);
-    break;
-
-  }
+  setDayDiscount();
 }
 
 
